2c15ac05f56528fb5fc388117f2cf8f330d6deb9
hous3m4ster <hous3m4ster@protonmail.com>
2016-11-08 14:01:43 +0100

    :neckbeard:

--- core/handler.php
+++ core/handler.php
@@ -1,4 +1,12 @@
 <?php
 //TODO
 include("./core/server.core.php");
+switch($_GET['page']){
+    case 'test':
+        print "Hi";
+        break;
+    case 'login':
+        print 'Login';
+        break;
+}
 ?>
